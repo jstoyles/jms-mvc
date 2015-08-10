0f0faee966e13d73defe8f97205c35707c466472
Jason Stoyles <jason.stoyles@gmail.com>
2015-08-10 18:13:37 -0400

    Create about.php

---
+++ controllers/about.php
@@ -0,0 +1,15 @@
+<?php
+
+class About_Controller
+{
+	public $viewfile = 'about'; //Must be defined with same name as view and model templates
+
+	public function main()
+	{
+		//Create a new view and pass it our template
+		$view = new View(strtolower($this->viewfile));
+
+		//Assign Page Variable
+		$view->assign('var1', 'About page variable.');
+	}
+}
