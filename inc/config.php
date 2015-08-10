7cb595fd160a6194d1fb820883b4721ced8e8832
Jason Stoyles <jason.stoyles@gmail.com>
2015-08-10 18:18:36 -0400

    Create config.php

---
+++ inc/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define("DB_TYPE","MySQL");
+define("MYSQL_HOST", "localhost");
+define("MYSQL_USER", "<username>");
+define("MYSQL_PASSWORD", "<password>");
+define("MYSQL_DB", "<database name>");
+
+?>
