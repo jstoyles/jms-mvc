85bd4caa40448a0ed65fc5db9c23e1b895e6ebde
Jason Stoyles <jason.stoyles@gmail.com>
2015-08-10 18:16:43 -0400

    Create _template.php

---
+++ views/_template.php
@@ -0,0 +1,7 @@
+<?php foreach($data['content'] as $d): ?>
+<p><?=$d['field1']?> | <?=$d['field2']?></p>
+<?php endforeach ?>
+
+<?php foreach($data['content2'] as $d): ?>
+<p><?=$d['field1']?> | <?=$d['field2']?></p>
+<?php endforeach ?>
