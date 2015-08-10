dcfbfa916f5d419ad34525ab85e8d2bbf2937f4e
Jason Stoyles <jason.stoyles@gmail.com>
2015-08-10 18:19:11 -0400

    Create header.php

---
+++ inc/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title><?=PAGE_TITLE?></title>
+
+    <meta name="description" content="<?=META_DESCRIPTION?>" />
+	<meta name="keywords" content="<?=META_KEYWORDS?>" />
+
+    <link rel="shortcut icon" href="/favicon.ico" />
+	<link media="all" rel="stylesheet" type="text/css" href="/css/global.css" />
+
+	<script type="text/javascript" src="/js/jquery-1.7.2.min.js"></script>
+
+</head>
+<body>
+
+<h1>Hello World!</h1>
